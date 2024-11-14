fe04824bcf89d6974277cbdabf4012ff5aba9cc1
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2024-11-14 09:25:51 +0530

    /.

--- server/index.js
+++ server/index.js
@@ -19,9 +19,20 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+const allowedOrigins = [
+  "https://coal-mines-worker-safety-website-ui.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "https://coal-mines-worker-safety-website-ui.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
